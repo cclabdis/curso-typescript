7fa8d8bcc68d2553dac29f14ecc2810f293032e8
cclabdis <cclabdis@gmail.com>
2023-09-13 19:24:16 -0300

    fix

--- aula-ts-04/src/exercicio4.ts
+++ aula-ts-04/src/exercicio4.ts
@@ -17,11 +17,17 @@ function showResult(result: number): void {
         console.log(`The result is ${result} and it's odd!`);
     }
 }
-// (() => {
-//     const num1 = prompt("First Number");
-//     const num2 = prompt("Second Number");
+(() => {
+    const num1 = Number(prompt("First Number"));
+    const num2 = Number(prompt("Second Number"));
     
-//     let result = sum(num1,num2);
-//     result += multiply(1,2);
-//     showResult(result);
-// })();
+    if(!isNaN(num1) && !isNaN(num2)){
+        let result = sum(num1,num2);
+        result += multiply(1,2);
+        showResult(result);
+
+    } else{
+        console.log("Não é número")
+    }
+   
+})();
---
+++ aula-ts-10/src/generics.ts
@@ -0,0 +1,7 @@
+type Collection = {
+    content: string[]
+}
+
+const collection: Collection = {
+    content: ["card1", "card2", "card3"]
+}
