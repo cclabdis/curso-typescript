3a7b441fade9ce59374d043591afcdbff37e04ee
cclabdis <cclabdis@gmail.com>
2023-09-13 16:31:48 -0300

    exercicio 5

--- aula-ts-04/src/exercicio4.ts
+++ aula-ts-04/src/exercicio4.ts
@@ -17,11 +17,11 @@ function showResult(result: number): void {
         console.log(`The result is ${result} and it's odd!`);
     }
 }
-(() => {
-    const num1 = prompt("First Number");
-    const num2 = prompt("Second Number");
+// (() => {
+//     const num1 = prompt("First Number");
+//     const num2 = prompt("Second Number");
     
-    let result = sum(num1,num2);
-    result += multiply(1,2);
-    showResult(result);
-})();
+//     let result = sum(num1,num2);
+//     result += multiply(1,2);
+//     showResult(result);
+// })();
---
+++ aula-ts-05/src/index.ts
@@ -0,0 +1,16 @@
+const numbers: number[] = [1, 2, 3, 4, 5, 6];
+const booleanos: boolean[] = [true, true, false, true];
+const strings: string[] = ["cachorro", "gato", "passarinho"]
+
+const number2: Array<number> = [1, 2, 3, 4, 5, 6];
+const booleanos2: Array<boolean> = [true, true, false, true];
+const strings2: Array<string> = ["cachorro", "gato", "passarinho"]
+
+const mix: (string | number)[] = ["cachorro", 2, "passarinho", 5]
+const mix2: Array<string | number> = ["cachorro", 2, "passarinho", 5]
+
+
+//tupla//tuple
+
+let tuple: [number, string, number]
+tuple = [1, "dois", 3]
